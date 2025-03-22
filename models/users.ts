05792dcc72cc9b88c08ae3efe3c5f5c44d1cbfb5
Marco Sa <marco.sa@md3.pt>
2025-03-22 01:36:19 +0000

    feat: add UserSchema for user validation and update Users type definition

--- models/users.ts
+++ models/users.ts
@@ -1,4 +1,11 @@
+import { t } from "elysia";
 import type { ObjectId } from "mongodb";
+
+export const UserSchema = t.Object({
+  _id: t.String(),
+  email: t.String({ format: "email" }),
+  password: t.String(),
+});
 
 export interface User {
   _id: ObjectId;
@@ -6,4 +13,4 @@ export interface User {
   password: string;
 }
 
-export type Users = Array<User>;
+export type Users = Array<Users>;
