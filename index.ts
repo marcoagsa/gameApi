b8cd5ab532d2a95a5ea1b64024c646e4defd8170
Marco Sa <marco.sa@md3.pt>
2025-03-31 18:23:24 +0100

    feat: integrate JWT authentication middleware and enhance route protection

--- index.ts
+++ index.ts
@@ -1,13 +1,37 @@
 import { Elysia, t } from "elysia";
-
 import { users } from "@routes/users";
 import { scores } from "@routes/scores";
 import { api } from "@routes/api";
 import { swaggerConfig } from "@models/swagger";
+import { jwtConfig } from "@models/jwt";
 
 const app = new Elysia()
   .use(swaggerConfig)
+  .use(jwtConfig)
   .group("", (app) => app.use(api).use(users).use(scores))
+  .onBeforeHandle(async ({ jwt, request, set }) => {
+    const publicRoutes = ["/signin", "/signup", "/swagger"];
+
+    if (publicRoutes.some((route) => request.url.includes(route))) {
+      return;
+    }
+
+    const authHeader = request.headers.get("authorization");
+
+    if (!authHeader?.startsWith("Bearer")) {
+      set.status = 401;
+      return { error: "No Token provider" };
+    }
+
+    const token = authHeader.slice(7);
+
+    console.log(`MSA 🔊 token:`, token);
+
+    if (!(await jwt.verify(token))) {
+      set.status = 401;
+      return { error: "Invalid Token" };
+    }
+  })
   .listen(Number(process.env.PORT) || 3000);
 
 console.log(`🔥 server working on ${app.server?.hostname}:${app.server?.port}`);
